c7986a14a8b1f8bfb560def1104eb2d7ec0981bf
ines-chung <41977846+ines-chung@users.noreply.github.com>
2020-08-06 20:49:57 +0100

    Add files via upload

--- application.py
+++ application.py
@@ -1,17 +1,18 @@
 import os
-
-from cs50 import SQL
+import cs50
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-
+import numpy as np
+import random
 
 
 #configure application
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.urandom(12).hex()
 
 '''HELPER FUNCTIONS:'''
 
@@ -57,11 +58,13 @@ def game_finished(board1, board2, correct_name):
 '''PREAMBLE'''
 
 db = cs50.SQL("sqlite:///characters.db")
-
+global all_names
 all_names = db.execute("SELECT name, id FROM characters")
+all_names = [person["name"] for person in all_names]
 list_of_features = ["hairpartition", "haircurly", "hat", "bald", "hairstuff", "hairlong", "hairginger", "hairwhite", "hairbrown", "hairblonde", "hairblack", "mouthbig", "nosebig", "cheeksred", "eyesblue", "sad", "hairfacial", "mustache", "beard", 'glasses', "earrings", "female"]
 all_ = (db.execute("SELECT * FROM characters"))
 
+global features_matrix
 features_matrix = []
 for feature in list_of_features:
     feature_lst = []
@@ -71,24 +74,151 @@ for feature in list_of_features:
     features_matrix.append(feature_lst)
 
 
+@app.route("/index", methods=["GET"])
+def index():
+    return render_template("index.html")
 
-
-
-@app.route("/play", )
-def play():
+@app.route("/", methods=["GET", "POST"])
+def choose():
     if request.method == "POST":
         #TODO #something about the sessions
-        users_choice = request.form.get("choose")
-        users_choice_index = np.where(user_board == users_choice) [0][0]
+        global all_names
+        #render the boards
+        global my_board
+        global user_board
+        global users_choice_index
+        global computers_choice_index
+        global computers_choice
+        my_board = np.array([[person, True] for person in all_names])
+        user_board = np.array([[person, True] for person in all_names])
+
+        session['myboard'] = my_board.tolist()
+
+        #users_choice = request.form.get("choose")
+        
+        for name in all_names:
+            if request.form.get("test") != None:
+                users_choice = request.form.get("test")
+                break
+
+
+        users_choice_index = np.where(user_board == users_choice)[0][0]
+        flash(f"Users choice index = {users_choice_index}")
+
         #computer chooses their card
         computers_choice_index = random.randint(0, len(my_board)-1)
         computers_choice = my_board[computers_choice_index][0]
 
-        #render the boards
-        my_board = np.array([[person["name"], True] for person in all_names])
-        user_board = np.array([[person["name"], True] for person in all_names])
+        # remind the user of their choice.
+        flash(f" you have successfully chosen {users_choice}")
+
         #redirect to the game.html page to start the questions
-        return render_template("game.html", users_choice = users_choice, )
+        return render_template("game.html", users_choice_index = users_choice_index, user_board = user_board.tolist(), computers_choice_index = computers_choice_index, my_board = my_board.tolist())
     else:
-        return render_template("play.html")
+        return render_template("choose.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        #TODOD
+        return render_template("register.html")
+    else:
+        return render_template("register.html")
+
+@app.route("/game", methods=["GET", "POST"])
+def game():
+    if request.method == "POST":
+            
+            
+        '''SET UP BOARDS AND CHOICES'''
+        
+        '''my_board = session.get('my_board', None) #request.args.get('my_board')
+        
+        user_board = request.args.get('user_board')
+
+        users_choice_index =  request.args.get('users_choice_index')
+
+        
+        computers_choice_index = request.args.get('computers_choice_index')'''
+        
+        global my_board
+        global user_board
+        global users_choice_index
+        global computers_choice_index
+        global computers_choice        
+
+        global list_of_features
+        global features_matrix
+
+        '''CHECK IF WON'''
+        if num_in(user_board) == 1 or num_in(my_board) == 1:
+            winner = game_finished(my_board, user_board, 0)
+            '''GO TO WINNING PAGE'''
+            flash(f"THE WINNER IS {winner}")
+            return render_template("choose.html")
+
+        '''USER'S TURN'''
+        question_index = request.form.get('answer')
+        question_index = int(np.where(list_of_features == question_index)[0])
+
+
+        #ADD A SECTION FOR IF THE USER INPUTS A NAME
+        #Ask the question
+        if features_matrix[question_index][computers_choice_index] == 1:
+            has_feature = 1
+        else:
+            has_feature = 0
+
+        #Update the user's board
+        for i in range(len(user_board)):
+            if features_matrix[question_index][i] != has_feature:
+                #eliminate from the board
+                user_board[i][1] = False
+
+        '''CHECK IF WON AGAIN'''
+        if num_in(user_board) == 1 or num_in(my_board) == 1:
+            winner = game_finished(my_board, user_board, 0)
+            '''GO TO WINNING PAGE'''
+            flash(f"THE WINNER IS {winner}")
+            return render_template("choose.html")
+
+
+        '''COMPUTERS TURN'''
+        comp_question_index = decision1(comp_features_matrix, my_board)
+
+        if features_matrix[comp_question_index][users_choice_index] == 1:
+            has_feature = 1
+
+            #print answer
+            #print(f"yes, your person has {list_of_features[comp_question_index]}")
+        else:
+            has_feature = 0
+            #print(f"no, your person doesnt have {list_of_features[comp_question_index]}")
+
+        #update the user's list of names
+        for i in range(len(my_board)):
+            if features_matrix[comp_question_index][i] != has_feature:
+                #eliminate from the board
+                my_board[i][1] = False
+
+        '''FLASH SOMETHING FOR THE RESULT OF THE USERS QUESTION'''
+        flash(f"No, I dont have{list_of_features[question_index]}")
+
+        '''FLASH THE COMPUTER"S QUESTION'''
+        flash(f"Does your person have{list_of_features[comp_question_index]}")
+
+        return render_template("game.html", users_choice_index = users_choice_index, user_board = user_board, computers_choice_index = computers_choice_index, my_board = my_board)
+
+    else:
+        '''SET UP BOARDS AND CHOICES'''
+        
+        
+        global my_board 
+        global user_board 
+        global users_choice_index 
+        global computers_choice_index
+        return render_template("game.html", users_choice_index = users_choice_index, user_board = user_board, computers_choice_index = computers_choice_index, my_board = my_board)
+
+
+
 
